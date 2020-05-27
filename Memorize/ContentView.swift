598ec175381f4dd475875d467500fcf5beb792f8
L Rettberg <lrettberg@outlook.com>
2020-05-27 12:49:18 -0500

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Lance Rettberg (39119312) on 5/27/20.
+//  Copyright © 2020 Lance Rettberg (19663346). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
